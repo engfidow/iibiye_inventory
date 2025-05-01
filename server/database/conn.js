27b55684ee8b3314c908a065f473dfed2ec3df84
Abdullahi Yusuf Ahmed Fidow <96941452+engfidow@users.noreply.github.com>
2025-05-01 07:59:11 -0700

    Update conn.js

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://root:JuCqeKbyq3SDEgFu@flash.dmifvb3.mongodb.net/?retryWrites=true&w=majority&appName=flash'; // Replace 'yourDatabaseName' with your actual database name
+const MONGO_URI = 'mongodb+srv://root:VGUOlGXxhjTNFzLU@cluster0.jzlb96b.mongodb.net/smart_trolly?retryWrites=true&w=majority&appName=Cluster0'; // Replace 'yourDatabaseName' with your actual database name
 
 mongoose.connect(MONGO_URI, )
   .then(() => console.log('Successfully connected to MongoDB✅'))
